9306ef1b5b324d02fa4897fb47da1222d80b15b2
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-06-16 17:07:40 +0900

    feat: life game via String

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,16 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+
+use std::assert_eq;
+
 use wasm_bindgen_test::*;
+use wasm_game_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn test_pass() {
+    let uni = Universe::new();
+    assert_eq!(uni.render().len(), 12352);
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,18 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new(64, 32);
+
+// コールバックループ
+const renderLoop = () => {
+    // テキストエリアにwasm空間からメモリをコピー
+    pre.textContent = universe.render();
+    // 更新
+    universe.tick();
+
+    // ループ再帰
+    requestAnimationFrame(renderLoop);
+};
+
+// ループ開始トリガー
+requestAnimationFrame(renderLoop);
