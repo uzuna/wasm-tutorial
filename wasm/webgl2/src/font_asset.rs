810602238b4cb6fe691a66ddf6815fbd66863b96
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-08-16 23:27:39 +0900

    fix(webgl2): 圧縮埋め込み実験。効果がないことを確認した

--- wasm/webgl2/src/font_asset.rs
+++ wasm/webgl2/src/font_asset.rs
@@ -3,20 +3,43 @@
 use wasm_bindgen::JsError;
 use web_sys::WebGlTexture;
 
-use crate::{
-    error::*,
-    font::{Font, FontTextureDetail},
-    gl,
-};
+use crate::{error::*, font::Font, gl};
 
-// フォント画像と位置情報のJSONを埋め込む
-// bmpだと400KB程度だが、DSS圧縮で60KB程度になることが期待される
-// 輝度情報だけなら100KB程度
-const FONT_IMAGE: &[u8] = include_bytes!("../testdata/Ubuntu_Mono_64px.lum");
-const FONT_JSON: &str = include_str!("../testdata/Ubuntu_Mono_64px.json");
+#[cfg(not(feature = "font-asset-compress"))]
+mod inner {
+    use crate::{error::*, font::FontTextureDetail};
+    // フォント画像と位置情報のJSONを埋め込む
+    // bmpだと400KB程度だが、DSS圧縮で60KB程度になることが期待される
+    // 輝度情報だけなら100KB程度
+    const FONT_IMAGE: &[u8] = include_bytes!("../testdata/Ubuntu_Mono_64px.lum");
+    const FONT_JSON: &str = include_str!("../testdata/Ubuntu_Mono_64px.json");
+
+    pub(crate) fn load() -> Result<(FontTextureDetail, &'static [u8])> {
+        let detail: FontTextureDetail = serde_json::from_str(FONT_JSON)?;
+        Ok((detail, FONT_IMAGE))
+    }
+}
+
+// zstd圧縮を使う。
+// ただし、200KB程度の画像データの場合はほとんど圧縮効果がなく、zstd実装分増えた
+// plotバイナリで比較。261763B -> 264052B
+#[cfg(feature = "font-asset-compress")]
+mod inner {
+    use crate::{error::*, font::FontTextureDetail};
+    pub(crate) fn load() -> Result<(FontTextureDetail, Vec<u8>)> {
+        let detail: FontTextureDetail = serde_json::from_slice(
+            &include_bytes_zstd::include_bytes_zstd!("testdata/Ubuntu_Mono_64px.json", 19),
+        )?;
+        Ok((
+            detail,
+            include_bytes_zstd::include_bytes_zstd!("testdata/Ubuntu_Mono_64px.lum", 19),
+        ))
+    }
+}
 
 pub fn load(gl: &gl) -> Result<Font> {
-    let detail: FontTextureDetail = serde_json::from_str(FONT_JSON).unwrap();
+    let (detail, image) = inner::load()?;
+
     let texture = gl
         .create_texture()
         .ok_or(JsError::new("Failed to create texture"))?;
@@ -52,7 +75,7 @@ pub fn load(gl: &gl) -> Result<Font> {
         border,
         gl::LUMINANCE,
         gl::UNSIGNED_BYTE,
-        Some(FONT_IMAGE),
+        Some(image),
     )
     .expect("Failed to set texture image");
 
