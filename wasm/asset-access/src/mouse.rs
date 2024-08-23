450d93eacbe0a9c024b2202b279ffb689534092a
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-08-23 22:28:06 +0900

    feat(asset): WheelEventの取得を実装

--- wasm/asset-access/src/mouse.rs
+++ wasm/asset-access/src/mouse.rs
@@ -1,10 +1,14 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::{RefCell, RefMut},
+    rc::Rc,
+};
 
 use futures_channel::mpsc::UnboundedSender;
 use fxhash::FxHashMap;
 use wasm_bindgen::prelude::*;
-use web_sys::MouseEvent;
+use web_sys::{MouseEvent, WheelEvent};
 
+/// マウス座標を保持、計算する構造体
 #[derive(Debug, Default, Clone, Copy)]
 pub struct Point {
     pub x: f32,
@@ -45,18 +49,29 @@ impl std::ops::Mul<f32> for Point {
     }
 }
 
+/// ホイールの移動量を保持する構造体
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Wheel {
+    pub x: f32,
+    pub y: f32,
+}
+
+/// モジュール外にマウスとホイールの状態を通知するメッセージ
 #[derive(Debug, Clone, Copy)]
 pub struct MouseMessage {
     pub pos: Point,
+    pub wheel: Wheel,
     pub down: bool,
 }
 
+// マウスの状態を保持する構造体
 #[derive(Debug, Default)]
 struct MouseState {
     offset_c: Point,
     area_c: Point,
     down: bool,
     pos: Point,
+    wheel: Wheel,
 }
 
 impl MouseState {
@@ -88,6 +103,13 @@ impl MouseState {
         self.pos = pos;
     }
 
+    fn update_wheel(&mut self, delta_x: f32, delta_y: f32) {
+        self.wheel = Wheel {
+            x: delta_x,
+            y: delta_y,
+        };
+    }
+
     fn mouse_down(&mut self) {
         self.down = true;
     }
@@ -95,6 +117,7 @@ impl MouseState {
     fn mouse_up(&mut self) {
         self.down = false;
     }
+
     fn msg(&self) -> MouseMessage {
         // マウス座標をOpenGL空間に変換
         let mut mouse_pos = (self.pos - self.offset_c - self.area_c / 2.) / self.area_c * 2.;
@@ -102,15 +125,25 @@ impl MouseState {
 
         MouseMessage {
             pos: mouse_pos,
+            wheel: self.wheel,
             down: self.down,
         }
     }
+
+    fn fetch_msg(&mut self) -> MouseMessage {
+        let msg = self.msg();
+        // wheelは継続する情報ではないのでリセット
+        self.wheel = Wheel::default();
+        msg
+    }
 }
 
+/// マウスイベントを処理する構造体
 pub struct MouseEventHandler {
     canvas: web_sys::HtmlCanvasElement,
     sender: UnboundedSender<MouseMessage>,
-    closures: FxHashMap<String, Closure<dyn FnMut(MouseEvent)>>,
+    mouse_closures: FxHashMap<String, Closure<dyn FnMut(MouseEvent)>>,
+    wheel_closures: FxHashMap<String, Closure<dyn FnMut(WheelEvent)>>,
     state: Rc<RefCell<MouseState>>,
 }
 
@@ -120,57 +153,79 @@ impl MouseEventHandler {
         Self {
             canvas,
             sender: tx,
-            closures: FxHashMap::default(),
+            mouse_closures: FxHashMap::default(),
+            wheel_closures: FxHashMap::default(),
             state,
         }
     }
 
     pub fn start(&mut self) {
-        let event_type = "mousedown";
-        let state = self.state.clone();
-        let mut tx = self.sender.clone();
-        let mouse_down = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            let mut state = state.borrow_mut();
+        // マウスの上げ下げイベントは位置と状態を更新
+        self.build_mouse_closure("mousedown", |(state, event)| {
             state.update_pos(event.client_x(), event.client_y());
             state.mouse_down();
-            tx.start_send(state.msg()).unwrap();
-        }) as Box<dyn FnMut(_)>);
-        self.canvas
-            .add_event_listener_with_callback(event_type, mouse_down.as_ref().unchecked_ref())
-            .unwrap();
-        self.closures.insert(event_type.to_string(), mouse_down);
+        });
 
-        let event_type = "mouseup";
-        let state = self.state.clone();
-        let mut tx = self.sender.clone();
-        let mouse_up = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            let mut state = state.borrow_mut();
+        self.build_mouse_closure("mouseup", |(state, event)| {
             state.update_pos(event.client_x(), event.client_y());
             state.mouse_up();
-            tx.start_send(state.msg()).unwrap();
-        }) as Box<dyn FnMut(_)>);
-        self.canvas
-            .add_event_listener_with_callback(event_type, mouse_up.as_ref().unchecked_ref())
-            .unwrap();
-        self.closures.insert(event_type.to_string(), mouse_up);
+        });
 
-        let event_type = "mousemove";
+        // マウス移動は移動のみを取得
+        self.build_mouse_closure("mousemove", |(state, event)| {
+            state.update_pos(event.client_x(), event.client_y());
+        });
+
+        // ホイールイベントはホイールの移動量を取得
+        self.build_wheel_closure("wheel", |(state, event)| {
+            state.update_wheel(event.delta_x() as f32, event.delta_y() as f32);
+        });
+    }
+
+    // マウスイベントのクロージャを登録
+    fn build_mouse_closure(
+        &mut self,
+        event_type: &str,
+        mut f: impl FnMut((&mut RefMut<MouseState>, MouseEvent)) + 'static,
+    ) {
         let state = self.state.clone();
         let mut tx = self.sender.clone();
-        let mouse_move = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+        let clusure = Closure::wrap(Box::new(move |event: MouseEvent| {
             let mut state = state.borrow_mut();
-            state.update_pos(event.client_x(), event.client_y());
+            f((&mut state, event));
             tx.start_send(state.msg()).unwrap();
-        }) as Box<dyn FnMut(_)>);
+        }) as Box<dyn FnMut(MouseEvent)>);
+
         self.canvas
-            .add_event_listener_with_callback(event_type, mouse_move.as_ref().unchecked_ref())
+            .add_event_listener_with_callback(event_type, clusure.as_ref().unchecked_ref())
             .unwrap();
-        self.closures.insert(event_type.to_string(), mouse_move);
+        self.mouse_closures.insert(event_type.to_string(), clusure);
+    }
+
+    // ホイールイベントのクロージャを登録
+    fn build_wheel_closure(
+        &mut self,
+        event_type: &str,
+        mut f: impl FnMut((&mut RefMut<MouseState>, WheelEvent)) + 'static,
+    ) {
+        let state = self.state.clone();
+        let mut tx = self.sender.clone();
+        let clusure = Closure::wrap(Box::new(move |event: WheelEvent| {
+            let mut state = state.borrow_mut();
+            f((&mut state, event));
+            // ホイールイベントは継続する情報ではないのでリセット
+            tx.start_send(state.fetch_msg()).unwrap();
+        }) as Box<dyn FnMut(WheelEvent)>);
+
+        self.canvas
+            .add_event_listener_with_callback(event_type, clusure.as_ref().unchecked_ref())
+            .unwrap();
+        self.wheel_closures.insert(event_type.to_string(), clusure);
     }
 
     #[allow(dead_code)]
     pub fn stop(&mut self) {
-        for (event_type, closure) in self.closures.drain() {
+        for (event_type, closure) in self.mouse_closures.drain() {
             self.canvas
                 .remove_event_listener_with_callback(
                     event_type.as_str(),
@@ -181,7 +236,10 @@ impl MouseEventHandler {
     }
 
     pub fn forget(self) {
-        for (_event_type, closure) in self.closures {
+        for (_event_type, closure) in self.mouse_closures {
+            closure.forget();
+        }
+        for (_event_type, closure) in self.wheel_closures {
             closure.forget();
         }
     }
