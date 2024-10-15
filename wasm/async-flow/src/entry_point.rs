70314287b52246ce961341badcfb455aa2dd3235
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-10-15 20:59:39 +0900

    feat(async-flow): add slider

--- wasm/async-flow/src/entry_point.rs
+++ wasm/async-flow/src/entry_point.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::*;
 use wasm_utils::{error::*, info};
 use web_sys::HtmlCanvasElement;
 
-use crate::input::{CheckBox, SubmitBtn};
+use crate::input::{CheckBox, SliderConfig, SliderInput, SubmitBtn};
 
 #[wasm_bindgen(start)]
 pub fn init() -> Result<()> {
@@ -26,6 +26,10 @@ pub fn start(canvas: HtmlCanvasElement) -> std::result::Result<(), JsValue> {
     let toggle_btn = CheckBox::new("toggle-btn", true)?;
     toggle_btn.start(tx)?;
 
+    let (tx, mut rx_f64) = futures::channel::mpsc::channel(1);
+    let slider = SliderInput::new("slider", SliderConfig::new(-1.0, 1.0, 0.1_f32, 0.1))?;
+    slider.start(tx)?;
+
     wasm_bindgen_futures::spawn_local(async move {
         info!("spawn_local");
         loop {
@@ -33,6 +37,9 @@ pub fn start(canvas: HtmlCanvasElement) -> std::result::Result<(), JsValue> {
             select! {
                 v = rx_tgl.next() => {
                     info!("toggle {v:?}");
+                }
+                v = rx_f64.next() => {
+                    info!("slider {v:?}");
                 }
                 _ = rx_sbm.next() => {
                     info!("submit");
--- wasm/async-flow/src/input.rs
+++ wasm/async-flow/src/input.rs
@@ -1,6 +1,7 @@
 use std::{
     cell::RefCell,
     rc::Rc,
+    str::FromStr,
     sync::atomic::{AtomicBool, Ordering},
 };
 
@@ -33,13 +34,15 @@ impl SubmitBtn {
         if contains(&self.id) {
             return Err(JsError::new("Closure already exists"));
         }
-        let ele = self.element.clone();
         let closure = Closure::wrap(Box::new(move || {
             // send message with sync
             tx.try_send(()).unwrap();
         }) as Box<dyn FnMut()>);
-        ele.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())
-            .unwrap();
+        add_event_listener(
+            self.element.dyn_ref::<web_sys::EventTarget>().unwrap(),
+            "click",
+            closure.as_ref(),
+        )?;
         // register closure
         insert(&self.id, closure);
         Ok(())
@@ -81,7 +84,6 @@ impl CheckBox {
         if contains(&self.id) {
             return Err(JsError::new("Closure already exists"));
         }
-        let ele = self.element.clone();
         let state = self.state.clone();
         let closure = Closure::wrap(Box::new(move || {
             let next = !state.borrow().load(Ordering::Relaxed);
@@ -90,8 +92,9 @@ impl CheckBox {
             // send message with sync
             tx.try_send(next).unwrap();
         }) as Box<dyn FnMut()>);
-        ele.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())
-            .unwrap();
+
+        self.element
+            .set_oninput(Some(closure.as_ref().unchecked_ref()));
         // register closure
         insert(&self.id, closure);
         Ok(())
@@ -101,6 +104,117 @@ impl CheckBox {
     pub fn apply(&self, value: bool) {
         self.state.borrow_mut().store(value, Ordering::Relaxed);
         self.element.set_checked(value);
+    }
+}
+
+pub trait SliderValue: Default + Clone + ToString + FromStr + 'static {}
+
+impl SliderValue for u8 {}
+impl SliderValue for u16 {}
+impl SliderValue for u32 {}
+impl SliderValue for u64 {}
+impl SliderValue for i8 {}
+impl SliderValue for i16 {}
+impl SliderValue for i32 {}
+impl SliderValue for i64 {}
+impl SliderValue for f32 {}
+impl SliderValue for f64 {}
+
+/// スライダエレメントの設定を作る
+#[derive(Debug, Clone)]
+pub struct SliderConfig<T>
+where
+    T: SliderValue,
+{
+    // 設定範囲とステップ、初期値を設定
+    pub min: T,
+    pub max: T,
+    pub step: T,
+    pub default: T,
+}
+
+impl<T> SliderConfig<T>
+where
+    T: SliderValue,
+{
+    pub fn new(min: T, max: T, step: T, default: T) -> Self {
+        Self {
+            min,
+            max,
+            step,
+            default,
+        }
+    }
+
+    fn apply(&self, ele: &web_sys::HtmlInputElement) {
+        ele.set_min(&self.min.to_string());
+        ele.set_max(&self.max.to_string());
+        ele.set_step(&self.step.to_string());
+        ele.set_value(&self.default.to_string());
+    }
+}
+
+pub struct SliderInput<T>
+where
+    T: SliderValue,
+{
+    id: String,
+    element: web_sys::HtmlInputElement,
+    state: Rc<RefCell<T>>,
+}
+
+impl<T> SliderInput<T>
+where
+    T: SliderValue,
+{
+    pub fn new(id: &str, config: SliderConfig<T>) -> Result<Self> {
+        let element = get_html_element::<web_sys::HtmlInputElement>(id)?;
+        config.apply(&element);
+        let state = Rc::new(RefCell::new(config.default.clone()));
+
+        let s = Self {
+            id: id.to_string(),
+            element,
+            state,
+        };
+        s.init();
+
+        Ok(s)
+    }
+
+    // 自身の状態とHTML要素の状態を同期する
+    pub fn init(&self) {
+        let value = self.state.borrow().to_string();
+        self.element.set_value(&value);
+    }
+
+    /// イベントリスナーを登録する
+    pub fn start(&self, mut tx: futures::channel::mpsc::Sender<T>) -> Result<()> {
+        // check closure
+        if contains(&self.id) {
+            return Err(JsError::new("Closure already exists"));
+        }
+        let ele = self.element.clone();
+        let state = self.state.clone();
+        let closure = Closure::wrap(Box::new(move || {
+            let value = match ele.value().parse::<T>() {
+                Ok(v) => v,
+                Err(_) => return,
+            };
+            *state.borrow_mut() = value.clone();
+            // send message with sync
+            tx.try_send(value).unwrap();
+        }) as Box<dyn FnMut()>);
+        self.element
+            .set_oninput(Some(closure.as_ref().unchecked_ref()));
+        insert(&self.id, closure);
+        Ok(())
+    }
+
+    /// プログラム側から状態を変更する
+    pub fn apply(&self, value: T) {
+        self.element.set_value(&value.to_string());
+        *self.state.borrow_mut() = value;
     }
 }
 
@@ -126,4 +240,16 @@ where
         .ok_or(JsError::new("Failed to get element"))?
         .dyn_into::<T>()
         .map_err(|_| JsError::new("Failed to convert to HtmlButtonElement"))
+}
+
+/// イベントリスナーを登録する
+fn add_event_listener(
+    element: &web_sys::EventTarget,
+    event: &str,
+    callback: &JsValue,
+) -> Result<()> {
+    element
+        .add_event_listener_with_callback(event, callback.unchecked_ref())
+        .map_err(|_| JsError::new("Failed to add event listener"))?;
+    Ok(())
 }
