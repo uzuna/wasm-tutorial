598e1bd4bf905e0378a360452e5d8061cb7e167d
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-10-16 16:17:57 +0900

    feat(async-flow): テキスト表示の実装とInputへの対応

--- wasm/async-flow/src/entry_point.rs
+++ wasm/async-flow/src/entry_point.rs
@@ -1,7 +1,13 @@
+use std::rc::Rc;
+
 use futures::StreamExt;
 use wasm_bindgen::prelude::*;
-use wasm_utils::{error::*, info};
+use wasm_utils::{animation::AnimationLoop, error::*, info};
 use web_sys::HtmlCanvasElement;
+use webgl2::{
+    context::{Context, COLOR_BLACK},
+    font::{Align, TextShader},
+};
 
 use crate::ui::{first, second};
 
@@ -13,10 +19,22 @@ pub fn init() -> Result<()> {
 
 #[wasm_bindgen]
 pub fn start(canvas: HtmlCanvasElement) -> std::result::Result<(), JsValue> {
-    canvas.set_width(1024);
-    canvas.set_height(768);
+    canvas.set_width(512);
+    canvas.set_height(384);
 
     info!("start");
+
+    // テキスト表示
+    let ctx = Context::new(canvas, COLOR_BLACK)?;
+    let gl = ctx.gl().clone();
+    let viewport = ctx.viewport();
+    let ts = TextShader::new(&ctx)?;
+    let font = webgl2::font::embed::load(&ctx)?;
+    let mut text = font.text_by_capacity(60, Align::left_bottom());
+    let mat = viewport.font_mat(0, 128, 16.0);
+    ts.local_mat(&mat);
+    let tv = Rc::new(ts.create_vbo(&text)?);
+    let tv_clone = tv.clone();
 
     let (ui1, mut rx1) = crate::ui::first::start()?;
     let (ui2, mut rx2) = crate::ui::second::start()?;
@@ -50,11 +68,26 @@ pub fn start(canvas: HtmlCanvasElement) -> std::result::Result<(), JsValue> {
                 second::Event::Select1(second::OptionMode::Off) => {
                     ui2.apply(second::Event::Select2(second::OptionStrength::Off));
                 }
+                second::Event::Text(t) => {
+                    text.update_text(&t);
+                    text.apply_to_vao(tv_clone.as_ref());
+                }
                 _ => {}
             }
         }
         info!("exit");
     });
+
+    // Canvasの描画
+    let mut a = AnimationLoop::new(move |_time_msec| {
+        webgl2::context::gl_clear_color(&gl, webgl2::context::COLOR_BLACK);
+        ts.draw(&tv);
+        Ok(())
+    });
+    a.start();
+    a.forget();
+
+    info!("start() done");
 
     Ok(())
 }
