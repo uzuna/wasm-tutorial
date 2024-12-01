19abbd5185c9017945cfc1375f72c45fe741a256
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-12-01 15:53:16 +0900

    fix: indexで有効なサービスに限定

--- wasm/blending/src/entry_point.rs
+++ wasm/blending/src/entry_point.rs
@@ -2,12 +2,9 @@ use std::{cell::RefCell, rc::Rc};
 
 use nalgebra::{Matrix3, Vector2};
 use wasm_bindgen::{convert::IntoWasmAbi, prelude::*};
-use wasm_utils::{animation::AnimationLoop, error::*, info};
-use web_sys::{HtmlCanvasElement, WebGlBuffer, WebGlProgram};
-use webgl2::{
-    blend::BlendMode, context::gl_clear_color, gl, program::compile_program,
-    shader::texture::TextureShader, texture::color_texture, vertex::buffer_data_f32,
-};
+use wasm_utils::{animation::AnimationLoop, error::*};
+use web_sys::HtmlCanvasElement;
+use webgl2::{blend::BlendMode, context::gl_clear_color};
 
 use crate::shader::SingleColorShaderGl1;
 
@@ -185,131 +182,4 @@ pub fn start(canvas: HtmlCanvasElement) -> std::result::Result<GlContext, JsValu
     a.forget();
 
     Ok(ctx)
-}
-
-#[wasm_bindgen]
-pub fn start_webgl2_texture(canvas: HtmlCanvasElement) -> std::result::Result<GlContext, JsValue> {
-    let width = 500;
-    let height = 300;
-    canvas.set_width(width);
-    canvas.set_height(height);
-    let local_mat = LocalMat::new(width as f32 / height as f32);
-
-    let ctx = webgl2::context::Context::new(canvas, BG_COLOR)?;
-    let gl = ctx.gl().clone();
-    let s = TextureShader::new(&ctx)?;
-
-    let r = SingleColorShaderGl1::UNIT_RECT;
-    let vao = s.create_vao(&r)?;
-
-    let t_r = color_texture(&gl, [255, 0, 0, 128])?;
-    let t_g = color_texture(&gl, [0, 255, 0, 128])?;
-    let t_b = color_texture(&gl, [0, 0, 255, 255])?;
-
-    let u = s.uniform();
-
-    u.set_mat(Matrix3::identity().append_nonuniform_scaling(&Vector2::new(1.0, 0.1)));
-    s.draw(&vao, &t_b);
-
-    u.set_mat(local_mat.with_translation(-0.5, -0.5));
-    s.draw(&vao, &t_r);
-
-    u.set_mat(local_mat.with_translation(0.5, 0.5));
-    s.draw(&vao, &t_g);
-
-    let ctx = GlContext::new(BlendMode::Alpha);
-    let ctx_clone = ctx.clone();
-
-    let mut a = AnimationLoop::new(move |_| {
-        let u = s.uniform();
-
-        // 背景色を描画。Canvasの影響を可視化するために青線をAlphaブレンドで描画
-        BlendMode::Alpha.enable(&gl);
-        gl_clear_color(&gl, BG_COLOR);
-        u.set_mat(Matrix3::identity().append_nonuniform_scaling(&Vector2::new(1.0, 0.1)));
-        s.draw(&vao, &t_b);
-
-        ctx_clone.blend.borrow().enable(&gl);
-        u.set_mat(local_mat.with_translation(-0.5, -0.5));
-        s.draw(&vao, &t_r);
-
-        u.set_mat(local_mat.with_translation(0.5, 0.5));
-        s.draw(&vao, &t_g);
-        Ok(())
-    });
-    a.start();
-    a.forget();
-
-    Ok(ctx)
-}
-
-#[wasm_bindgen]
-pub fn get_context_rs(canvas: HtmlCanvasElement) -> Result<gl> {
-    let gl = canvas
-        .get_context("experimental-webgl")
-        .map_err(|_| JsError::new("Failed to get_context(webgl)"))?
-        .ok_or(JsError::new("Failed to get WebGlRenderingContext Object"))?
-        .dyn_into::<gl>()
-        .map_err(|_| JsError::new("Failed to cast to WebGlRenderingContext"))?;
-    Ok(gl)
-}
-
-#[wasm_bindgen]
-pub fn create_program_rs(gl: gl) -> Result<WebGlProgram> {
-    compile_program(&gl, SingleColorShaderGl1::VERT, SingleColorShaderGl1::FRAG)
-}
-
-#[wasm_bindgen]
-pub fn get_attr_location_rs(gl: gl, prg: &WebGlProgram, name: &str) -> i32 {
-    gl.get_attrib_location(prg, name)
-}
-
-#[wasm_bindgen]
-pub fn create_vbo_rs(gl: gl, data: &[f32]) -> std::result::Result<WebGlBuffer, JsValue> {
-    let vbo = gl.create_buffer().ok_or("failed to create buffer")?;
-    gl.bind_buffer(gl::ARRAY_BUFFER, Some(&vbo));
-    info!("bind_buffer {:?}", gl.get_error());
-    buffer_data_f32(&gl, gl::ARRAY_BUFFER, data, gl::STATIC_DRAW);
-    info!("buffer_data_with_array_buffer_view {:?}", gl.get_error());
-    gl.bind_buffer(gl::ARRAY_BUFFER, None);
-    Ok(vbo)
-}
-
-#[wasm_bindgen]
-pub fn bind_buffer_rs(gl: gl, attr: u32, vbo: &WebGlBuffer) -> std::result::Result<(), JsValue> {
-    gl.bind_buffer(gl::ARRAY_BUFFER, Some(vbo));
-    gl.enable_vertex_attrib_array(attr);
-    gl.vertex_attrib_pointer_with_i32(attr, 2, gl::FLOAT, false, 0, 0);
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn clear_canvas_rs(gl: gl) -> std::result::Result<(), JsValue> {
-    gl.clear_color(0.0, 0.0, 0.75, 1.0);
-    gl.clear_depth(1.0);
-    gl.clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn set_uniform_color_rs(
-    gl: gl,
-    prg: &WebGlProgram,
-    color: js_sys::Float32Array,
-) -> std::result::Result<(), JsValue> {
-    let u_color = gl.get_uniform_location(prg, "u_color");
-    gl.uniform4fv_with_f32_sequence(u_color.as_ref(), &color);
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn draw_rs(gl: gl) -> std::result::Result<(), JsValue> {
-    gl.draw_arrays(gl::TRIANGLE_STRIP, 0, 4);
-    Ok(())
-}
-
-#[wasm_bindgen]
-pub fn setup_depth_test_rs(gl: gl) {
-    gl.enable(gl::DEPTH_TEST);
-    gl.depth_func(gl::LEQUAL);
 }
