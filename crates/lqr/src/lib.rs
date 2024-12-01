f27f5e2bc8d41150035b27ffb06c5489fd7bc541
Fumiya Fujinaka <uzuna.kf@gmail.com>
2024-12-01 15:39:10 +0900

    memo

--- crates/lqr/src/lib.rs
+++ crates/lqr/src/lib.rs
@@ -27,7 +27,7 @@ pub mod sim {
 
 #[cfg(test)]
 mod tests {
-    use ndarray::{array, Array2};
+    use ndarray::{array, Array1, Array2, Array3};
 
     #[test]
     fn test_dare() {
@@ -41,5 +41,35 @@ mod tests {
         let p: Array2<f32> = Array2::eye(2);
         let z = &a.t() * &p + &p * &a - &p * &b.t() * &b * &p + &c.t() * &c;
         println!("{:?}", z);
+    }
+
+    #[test]
+    fn test_troc() {
+        // https://qiita.com/harmegiddo/items/ddd33f40d5e368a210df
+
+        pub struct Troc {
+            pos: Array1<f32>,
+            vel: Array1<f32>,
+        }
+
+        // トロッコの新地を作る部分
+        // 位置と加速度
+        let x: Array2<f32> = array!([0.], [0.]);
+
+        // 運動方程式
+        let dt = 0.1_f32;
+        let f = array![[1., dt], [0., 1.]];
+
+        // ノイズモデル
+        let g = array![[0.5 * dt.powi(2)], [dt]];
+
+        let w = 0.0_32;
+
+        let mut x_t = x.clone();
+        for _ in 0..10 {
+            // 状態更新、
+            x_t = (&f * &x_t) + (&g);
+            println!("{:?}", x_t);
+        }
     }
 }
